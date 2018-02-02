f180a05f25f76b90aeb162ba23ca67d1801a1c75
AnastasiiaKugach <35683598+AnastasiiaKugach@users.noreply.github.com>
2018-02-02 17:31:40 +0300

    Новые функции

--- sandbox/src/main/java/ru/TEST/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/TEST/sandbox/FirstProgram.java
@@ -3,12 +3,23 @@ package ru.test.sandbox;
 public class FirstProgram {
 
     public static void main(String[] args) {
-        String somebody = "world!";
-        System.out.println("Hello, " + somebody);
-
-        int l = 6;
-        int s = l*l;
-        System.out.println ("Площадь квадрата  со стороной " + l + " = " + s);
+       hello("world");
+       double l = 5;
+       double a = 2;
+       double b = 3;
+       System.out.println ("Площадь квадрата со стороной "+ l + " = " + area(l));
+       System.out.println ("Площадь прямоугольника со сторонами "+ a + " и " + b +  " = " + area(a, b));
     }
 
+    public static void hello(String smb) {
+       System.out.println("Hello, " + smb + " !" );
+    }
+
+    public static double area (double l){
+      return l*l;
+    }
+
+    public static double area (double a, double b){
+      return a*b;
+    }
 }
