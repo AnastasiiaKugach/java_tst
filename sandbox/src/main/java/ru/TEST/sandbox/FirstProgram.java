5ab7a4d4b9fc927fe751470b9fe42269a538fa6b
AnastasiiaKugach <35683598+AnastasiiaKugach@users.noreply.github.com>
2018-02-02 19:11:08 +0300

    классы, конструкторы

--- sandbox/src/main/java/ru/TEST/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/TEST/sandbox/FirstProgram.java
@@ -3,23 +3,26 @@ package ru.test.sandbox;
 public class FirstProgram {
 
     public static void main(String[] args) {
-       hello("world");
-       double l = 5;
-       double a = 2;
-       double b = 3;
-       System.out.println ("Площадь квадрата со стороной "+ l + " = " + area(l));
-       System.out.println ("Площадь прямоугольника со сторонами "+ a + " и " + b +  " = " + area(a, b));
+
+      Square s = new Square(5);
+      //s.l = 5;
+
+      Rectangle r = new Rectangle(2,3);
+      r.a = 2;
+      r.b = 3;
+      System.out.println ("Площадь квадрата со стороной "+ s.l + " = " + area(s));
+      System.out.println ("Площадь прямоугольника со сторонами "+ r.a + " и " + r.b +  " = " + area(r));
     }
 
     public static void hello(String smb) {
        System.out.println("Hello, " + smb + " !" );
     }
 
-    public static double area (double l){
-      return l*l;
+    public static double area (Square s){
+      return s.l*s.l;
     }
 
-    public static double area (double a, double b){
-      return a*b;
+    public static double area (Rectangle r){
+      return r.a*r.b;
     }
 }
