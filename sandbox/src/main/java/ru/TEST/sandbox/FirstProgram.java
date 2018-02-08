b63075b57bb1be5dfa0d2b5747b2ca7532fba5b1
AnastasiiaKugach <35683598+AnastasiiaKugach@users.noreply.github.com>
2018-02-08 16:40:35 +0300

    TestNG

--- sandbox/src/main/java/ru/TEST/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/TEST/sandbox/FirstProgram.java
@@ -2,27 +2,16 @@ package ru.test.sandbox;
 
 public class FirstProgram {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-      Square s = new Square(5);
-      //s.l = 5;
+    Square s = new Square(5);
+    Rectangle r = new Rectangle(2, 3);
 
-      Rectangle r = new Rectangle(2,3);
-      r.a = 2;
-      r.b = 3;
-      System.out.println ("Площадь квадрата со стороной "+ s.l + " = " + area(s));
-      System.out.println ("Площадь прямоугольника со сторонами "+ r.a + " и " + r.b +  " = " + area(r));
-    }
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+  }
 
-    public static void hello(String smb) {
-       System.out.println("Hello, " + smb + " !" );
-    }
-
-    public static double area (Square s){
-      return s.l*s.l;
-    }
-
-    public static double area (Rectangle r){
-      return r.a*r.b;
-    }
+  public static void hello(String smb) {
+    System.out.println("Hello, " + smb + " !");
+  }
 }
